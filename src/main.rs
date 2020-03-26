eb34a34b028edabc2df09b128e3da65abb285102
YuhanLiin <linyuhan0315@hotmail.com>
2020-03-26 01:47:29 -0400

    Got essential crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use msp430_rt::entry;
+use msp430fr2x5x_hal as _;
+use panic_msp430 as _;
+
+#[entry]
+fn main() -> ! {
+    loop {}
 }
